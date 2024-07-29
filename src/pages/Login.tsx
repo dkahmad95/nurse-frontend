08d3edb04f2a0c102676e1939d7567c493c0ef9e
dkahmad95 <dkahmad95@gmail.com>
2024-07-29 14:26:17 +0300

    first commit

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -3,7 +3,7 @@ import LoginForm from "../Components/LoginForm"
 
 const Login = () => {
   return (
-    <main  className="flex flex-col justify-center py-12 xl:py-0 bg-accent">
+    <main  className="flex flex-col justify-center bg-accent">
            <div className="container mx-auto ">
            <LoginForm/>
            </div>
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -3,7 +3,7 @@ import RegisterForm from '@/Components/RegisterForm'
 
 const Register = () => {
   return (
-    <main  className=" flex flex-col justify-center py-12 xl:py-0 bg-accent">
+    <main  className=" flex flex-col justify-center  bg-accent">
     <div className="container mx-auto ">
     <RegisterForm/>
     </div>
